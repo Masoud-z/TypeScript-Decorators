4589ecc7f96d0a451db8fbb365c89ee83c03cf72
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-23 12:46:39 +0330

    Accessor and Parameter decorators

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,31 @@ function Log(target: any, propertyName: string | symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
 class Product {
   @Log
   title: string;
@@ -53,6 +78,7 @@ class Product {
     this.title = title;
     this._price = _price;
   }
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -60,7 +86,8 @@ class Product {
       throw new Error("Invalid price - Should be positive");
     }
   }
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
